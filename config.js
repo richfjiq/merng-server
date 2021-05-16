9ab25a6b9ef01c87eff5dc0d987ab078ec742f98
rfj1986 <rfjiq1986@gmail.com>
2021-05-15 20:22:00 -0500

    init

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://admin:9X7X9RgzIxl8qmMG@cluster0.a6kd9.mongodb.net/merng?retryWrites=true&w=majority",
+  SECRET_KEY: "clave src 124786 sjkdv sjhgdaf 187264",
+};
---
+++ graphql/resolvers/comments.js
@@ -0,0 +1,59 @@
+const { UserInputError, AuthenticationError } = require("apollo-server");
+
+const Post = require("../../models/Post");
+const { checkAuth } = require("../../util/check-auth");
+
+module.exports = {
+  Mutation: {
+    createComment: async (parent, { postId, body }, context, info) => {
+      const { username } = checkAuth(context);
+
+      if (body.trim() === "") {
+        throw new UserInputError("Empty comment", {
+          errors: {
+            body: "Comment body must not empty",
+          },
+        });
+      }
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        post.comments.unshift({
+          body,
+          username,
+          createdAt: new Date().toISOString(),
+        });
+
+        await post.save();
+
+        return post;
+      } else {
+        throw new UserInputError("Post not found");
+      }
+    },
+    deleteComment: async (parent, { postId, commentId }, context, info) => {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        const commentIndex = post.comments.findIndex(
+          (comment) => comment.id === commentId
+        );
+
+        if (post.comments[commentIndex].username === username) {
+          post.comments.splice(commentIndex, 1);
+
+          await post.save();
+
+          return post;
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } else {
+        throw new UserInputError("Post not found");
+      }
+    },
+  },
+};
